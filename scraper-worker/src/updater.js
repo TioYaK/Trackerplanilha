1a30d81d82d4898f1a722267f5190074f54d7145
YaK <pifot16@gmail.com>
2026-08-25 14:51:38 -0300

    fix(updater): use autostash on pull to avoid local cache files blocking updates

--- scraper-worker/src/updater.js
+++ scraper-worker/src/updater.js
@@ -10,7 +10,7 @@ export const checkForUpdates = async () => {
     // Se não estiver rodando como EXE compilado, usa o Git Pull clássico
     if (!process.pkg) {
         return new Promise((resolve) => {
-            exec('git pull', (error, stdout) => {
+            exec('git pull --autostash', (error, stdout) => {
                 if (stdout && !stdout.includes('Already up to date')) {
                     console.log('[UPDATER] Nova atualização do Git! Reiniciando...');
                     process.exit(0);
