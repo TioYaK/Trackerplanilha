8ffd4bc051d53c136b8671cfb41ee5ccf65da89b
YaK <pifot16@gmail.com>
2026-08-26 11:14:47 -0300

    fix(frontend): update uninstaller to use PowerShell instead of WMIC to properly kill zombie Chrome processes on Windows 11

--- frontend/public/download/Desinstalador_Worker.bat
+++ frontend/public/download/Desinstalador_Worker.bat
@@ -16,17 +16,20 @@ pause >nul
 
 echo.
 echo [1/3] Encerrando processos do Worker...
-wmic process where "name='cscript.exe' and commandline like '%%StartAuroriaWorker.vbs%%'" call terminate >nul 2>&1
-wmic process where "name='cmd.exe' and commandline like '%%loop.bat%%'" call terminate >nul 2>&1
-wmic process where "name='node.exe' and commandline like '%%src/index.js%%'" call terminate >nul 2>&1
-:: Mata processos do Chrome invisiveis que podem ter ficado orfaos (Puppeteer)
-wmic process where "name='chrome.exe' and commandline like '%%--remote-debugging-port%%'" call terminate >nul 2>&1
+powershell -Command "Get-CimInstance Win32_Process -Filter \"name='cscript.exe'\" | Where-Object { $_.CommandLine -match 'StartAuroriaWorker.vbs' } | Invoke-CimMethod -MethodName Terminate" >nul 2>&1
+powershell -Command "Get-CimInstance Win32_Process -Filter \"name='cmd.exe'\" | Where-Object { $_.CommandLine -match 'loop.bat' } | Invoke-CimMethod -MethodName Terminate" >nul 2>&1
+powershell -Command "Get-CimInstance Win32_Process -Filter \"name='node.exe'\" | Where-Object { $_.CommandLine -match 'src/index.js' -or $_.CommandLine -match 'AuroriaWorker' } | Invoke-CimMethod -MethodName Terminate" >nul 2>&1
+:: Mata processos do Chrome invisiveis que podem ter ficado orfaos (Puppeteer) sem fechar o Chrome principal do usuario
+powershell -Command "Get-CimInstance Win32_Process -Filter \"name='chrome.exe'\" | Where-Object { $_.CommandLine -match 'puppeteer' -or $_.CommandLine -match '--headless=new' } | Invoke-CimMethod -MethodName Terminate" >nul 2>&1
 
 echo [2/3] Removendo o auto-iniciar do Windows...
 del /F /Q "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\StartAuroriaWorker.vbs" >nul 2>&1
 
 echo [3/3] Apagando arquivos e pasta do Tracker...
+:: Tenta matar qualquer travamento remanescente antes de apagar a pasta
+powershell -Command "Start-Sleep -Seconds 2"
 rmdir /S /Q "%APPDATA%\AuroriaWorker" >nul 2>&1
+rmdir /S /Q "%APPDATA%\AuroriaWorker_*" >nul 2>&1
 
 echo.
 echo ========================================================
